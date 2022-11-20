ca91d65ee60b6c468bc9a258bd56e5f2bfcc4e2d
Keshav Shakti Singh <keshavshakti14@gmail.com>
2022-11-20 11:06:29 +0530

    first commit

---
+++ technical_support.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class technical_support
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int test = sc.nextInt();
+		while(test-->0){
+		    int n = sc.nextInt();
+		    char ch[] = sc.next().toCharArray();
+		    int q = 0;
+		    int a = 0;
+		    for(int i =0; i<n; i++){
+		        if(ch[i] == 'Q')q++;
+		        else q = Math.max(0, q-1);
+		    }
+		    if(q == 0){
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		    
+		}
+	}
+}
---
+++ watermelon.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class watermelon {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        if(test>2){
+            if(test % 2 == 0)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
